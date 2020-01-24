fefeb8b35cbac71fde5b82fda019459d1ad122f1
Castro <castro@linux.com>
2020-01-24 03:29:18 -0300

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,37 @@
+// Animate on Scroll
+new WOW().init();
+
+// Change menu color
+$(".navbar-toggler").click(function(){
+    $("nav").addClass('bg-dark');
+    $("nav").addClass('shadow');
+}); 
+
+// Collapse menu icon on link touch
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+}); 
+
+// On scroll
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+        $("nav").addClass('bg-dark');
+        $("nav").addClass('shadow');
+        $("nav").removeClass('py-4');
+    } else {
+        if ($(".navbar-toggler").is(":visible") == false) {
+            $("nav").removeClass('bg-dark');
+            $("nav").removeClass('shadow');
+            $("nav").addClass('py-4');
+        } else if ($("#navbarNav").is(":visible") == false) {
+            $("nav").removeClass('bg-dark');
+            $("nav").removeClass('shadow');
+            $("nav").addClass('py-4');
+        }
+    }   
+}
+
+// Get current year
+$("#date").text( (new Date).getFullYear() );
